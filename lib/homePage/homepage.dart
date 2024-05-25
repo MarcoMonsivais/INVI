7f7a0f59ec06edb408e084281ceb5961172ec5eb
marco_monsivais <marco_monsivais@hotmail.com>
2024-05-25 06:32:48 +0000

    assets

--- lib/homePage/homepage.dart
+++ lib/homePage/homepage.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -13,21 +10,64 @@ class _MyHomePageState extends State<MyHomePage> {
  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Index Page',
-            ),
-          ],
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          // Desktop layout
+          return Row(
+            children: [
+              Expanded(
+                flex: 6,
+                child: Image.asset(
+                  'assets/bg-login.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: EdgeInsets.all(32.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sign In',
+                      ),
+                      SizedBox(height: 16.0),
+                      // Add your login form here
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        } else {
+          // Mobile layout
+          return Column(
+            children: [
+              Image.asset(
+                'assets/bg-login.png',
+                fit: BoxFit.cover,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sign In',
+                    ),
+                    SizedBox(height: 16.0),
+                    // Add your login form here
+                  ],
+                ),
+              ),
+            ],
+          );
+        }
+      },
     );
   }
 }
